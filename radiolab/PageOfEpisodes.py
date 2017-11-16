91051ffb384f70a9b628f8a1e5202e67e6323e01
John Ludwig <johnludwigm@users.noreply.github.com>
2017-11-16 12:30:46 -0600

    Add docstrings

--- radiolab/PageOfEpisodes.py
+++ radiolab/PageOfEpisodes.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 class PageOfEpisodes:
+  """Class to parse pages in the Radiolab archive and get episodes."""
 
   def __init__(self, url):
     
@@ -15,6 +16,7 @@ class PageOfEpisodes:
       print("PageOfEpisodes-Exception occurred during initialization:\n\t%s" % exc)
 
   def getepisodes(self):
+    """Returns list of episodes on the page in chronological order."""
+    
     #Reversed because a page has the newest episodes at the top and oldest at the bottom.
     self.contents = reversed((self.soup).find_all("div", {"class": "series-item"}))
-    
