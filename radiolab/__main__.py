22e3628278e774926bdf7333fb02a84e66385155
John Ludwig <johnludwigm@users.noreply.github.com>
2017-11-16 12:55:54 -0600

    Add docstrings

--- radiolab/__main__.py
+++ radiolab/__main__.py
@@ -12,13 +12,17 @@ github_url = "https://raw.githubusercontent.com/johnludwigm/Radiolab/master/Radi
 
 import argparse
 def parse(args):
+  """Argument parser."""
   parser = argparse.ArgumentParser()
   parser.add_argument('-dl', '--download', nargs='*', default = '')
   parser.add_argument('-dest', '--destination', nargs='?', default='~')
   parser.add_argument('-dlall', '--downloadall')
   return vars(parser.parse_args(args[1:]))
 
+
 def radiolabpages(lastpage = 9999999):
+  """Generator yielding pages of Radiolab episodes.
+  lastpage defaults to 9999999, which defaults to the actual last page in the archive, which is usually 15."""
   reqobj = requests.get("http://www.radiolab.org/series/podcasts/%s" % lastpage)
   currentsoup = BeautifulSoup(reqobj.text, "html.parser")
 
@@ -31,23 +35,30 @@ def radiolabpages(lastpage = 9999999):
       yield PageOfEpisodes("http://www.radiolab.org/series/podcasts/")
     current -= 1
 
+    
 #Instead of running through all available pages in the Radiolab archives
 #to get the episodes and their links, the package by default will get the episodes
-#from a JSON file on my GitHub page.
-def fromgithub():
-  res = requests.get(github_url)
+#from a JSON file on my GitHub page. 
 
+def fromgithub():
+  """Returns JSON-style dictionary {episode-name: episodeurl.mp3}"""
+  res = requests.get(github_url)
   #transform res.content into a Python dictionary
   return eval(res.content)  
 
+
 def downloadall():
+  """Download all episodes listed in the JSON on my profile."""
   for episode, episode_url in fromgithub().items():
     try:
       downloadmp3(episode, episode_url)
     except Exception as exc:
       print("Unable to download %s:\n%s\n," % (episode, exc))
 
+      
 def generatejson(outputfilename):
+  """Creates the JSON for Radiolab episodes and saves it in text file `outputfilename`."""
+  
   from collections import OrderedDict
   from json import dumps
 
@@ -60,12 +71,14 @@ def generatejson(outputfilename):
       episode_to_url[episode.title] = episode.downloadurl
 
   jsondict = OrderedDict(sorted(episode_to_url.items(), key = lambda a: a[0]))
+  
   with open(outputfilename, "w", encoding = "utf-8") as fileobj:
     fileobj.write(dumps(jsondict, indent = "\t"))
 
 def generatemd(outputfilename):
+  """Creates markdown file detailing information for all available episodes."""
   if not outputfilename.endswith(".md"):
-    outputfilename = outputfilename + ".md"
+    outputfilename += ".md"
 
   with open(outputfilename, "w", encoding = "utf-8") as fileobj:
     for page in radiolabpages():
@@ -74,6 +87,7 @@ def generatemd(outputfilename):
         fileobj.write(episode.markdowninfo())
 
 def main(args = None):
+  """Main function that prompts user and handles arguments."""
   if args is None:
     args = argv
   
